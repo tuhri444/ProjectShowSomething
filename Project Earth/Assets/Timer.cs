3e7435b440166c0a986f4c5bfb7185bc63461951
ragingram2 <blazinram@gmail.com>
2020-05-06 16:22:18 +0200

    [Add] instruction text

---
+++ Project Earth/Assets/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    float waitTime = 5;
+    public float fadeTime = 1.0f;
+    float startTime = 0;
+    float newAlpha = 0;
+    bool canStartFade = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make the text move upwards
+        //transform.Translate(0, Time.deltaTime * 1.0f, 0);
+
+        //Compute and set the alpha value
+        if (Time.time - startTime > waitTime && !canStartFade)
+        {
+            startTime = Time.time;
+            canStartFade = true;
+        }
+        if(canStartFade)
+        {
+            newAlpha = 1.0f - (Time.time - startTime) / fadeTime;
+            GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, newAlpha);
+
+            //If alpha has decreased to zero, destroy this game object
+            if (newAlpha <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
