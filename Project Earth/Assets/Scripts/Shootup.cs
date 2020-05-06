509e0fd1c5a3c1f7dbfaa1b5566a506bb6af8541
Arjen de Aldrey <adealdrey1@gmail.com>
2020-05-06 12:14:58 +0200

    [FIX] hopefully fixed [ADD] we got horizontal mode too

--- Project Earth/Assets/Scripts/Shootup.cs
+++ Project Earth/Assets/Scripts/Shootup.cs
@@ -7,6 +7,9 @@ public class Shootup : MonoBehaviour
     [SerializeField] float SPEEEEEEEEEEEED;
     [SerializeField] float rotateSpeed;
     [SerializeField] ParticleSystem jetstream;
+    [SerializeField] ParticleSystem jetstream2;
+    [SerializeField] ParticleSystem jetstream3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +21,12 @@ public class Shootup : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.W))
         {
-            if (jetstream.isStopped) jetstream.Play(true);
+            jetstream.Play(true);
             GetComponent<Rigidbody>().AddForce(transform.up.normalized*SPEEEEEEEEEEEED);
         }
         else
         {
-            if (jetstream.isPlaying) jetstream.Stop(true);
+            jetstream.Stop(true);
         }
         if(Input.GetKey(KeyCode.D))
         {
@@ -32,6 +35,17 @@ public class Shootup : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(new Vector3(0, 0, 1), rotateSpeed);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            jetstream2.Play(true);
+            jetstream3.Play(true);
+            GetComponent<Rigidbody>().AddForce(transform.up.normalized * -SPEEEEEEEEEEEED);
+        }
+        else
+        {
+            jetstream2.Stop(true);
+            jetstream3.Stop(true);
         }
     }
 }
