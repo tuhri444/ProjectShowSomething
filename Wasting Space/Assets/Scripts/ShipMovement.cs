db85b8788c55316a676bd3d8aaca3180f577f914
ragingram2 <blazinram@gmail.com>
2020-05-18 20:47:52 +0200

    [ADDED] Smooth cam script and ship movement

---
+++ Wasting Space/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    //Serializable Fields
+    [SerializeField]
+    private float speed = 5;
+
+    //Non-Serializable Fields
+    private Vector2 move = new Vector2();
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        move = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        move *= 100*speed;
+        move *= Time.deltaTime;
+        rigidbody.AddForce(move);
+
+        //if (Mathf.Abs(move.magnitude) < .1f)
+        //{
+        //    rigidbody.velocity = Vector3.Lerp(rigidbody.velocity, Vector3.zero, 5f);
+        //}
+        Debug.Log(rigidbody.velocity);
+    }
+}
---
+++ Wasting Space/Assets/SmoothCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    [Range(0, .5f)]
+    private float time = .1f;
+    [SerializeField]
+    private float range = 10;
+    [SerializeField]
+    public Vector3 target_Offset;
+
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null)
+        {
+            target = transform;
+            Debug.Log("No trarget found");
+            return;
+        }
+        target_Offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 diff = target.position - transform.position;
+        if (diff.magnitude < range)
+        {
+            // Define a target position above and behind the target transform
+            Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
+
+            // Smoothly move the camera towards that target position
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, time);
+
+        }
+        else
+        {
+            transform.position = Vector3.Max(transform.position,new Vector3(target.position.x+diff.x,target.position.y+diff.y,-range));
+        }
+    }
+}
