643d5e333a47be8cd2d42e8a849e3c54299fdfcf
ragingram2 <blazinram@gmail.com>
2020-05-26 10:56:38 +0200

    [Change] Leaderboard

---
+++ Wasting Space/Assets/PopulateLeaderboard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateLeaderboard : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject LeaderboardSlotPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0;i<5;i++)
+        {
+            GameObject temp = Instantiate(LeaderboardSlotPrefab, transform);
+            string topScorer = PlayerPrefs.GetString("TopScorer" + i);
+            string[] split = topScorer.Split(',');
+            string name = split[0];
+            string scoreStr = split[0];
+            int score = int.Parse(scoreStr);
+            temp.GetComponent<LeaderboardSlot>().Rank = i+1;
+            temp.GetComponent<LeaderboardSlot>().Name = name;
+            temp.GetComponent<LeaderboardSlot>().Score = score;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
