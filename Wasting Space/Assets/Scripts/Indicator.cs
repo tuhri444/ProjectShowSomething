70853a4606777436ff70c6b24799669443376da6
ragingram2 <blazinram@gmail.com>
2020-06-08 22:33:14 +0200

    [Fix] Merge issues and [Add] Indicator

---
+++ Wasting Space/Assets/Scripts/Indicator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicator : MonoBehaviour
+{
+    public GameObject Ship;
+    public GameObject DropOff;
+    public Camera radar;
+    public RectTransform rectCanvas;
+
+    [SerializeField] private float distanceFromPlayer = 0.5f;
+
+    Vector3 diff;
+    public Vector3 dir;
+    Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DropOff = GameObject.Find("spacestation 2");
+        Canvas canvas = FindObjectOfType<Canvas>();
+        rectCanvas = canvas.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        diff = DropOff.transform.position- Ship.transform.position;
+        dir = diff.normalized * distanceFromPlayer;
+        if (diff.magnitude < 15)
+        {
+            transform.position = DropOff.transform.position; 
+        }
+        else
+        {
+            float oldZ = transform.position.z;
+            transform.position = Ship.transform.position + dir;
+            transform.localPosition = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+    }
+}
