33784ec23518d8f3bf846bb05a539900c2f3e086
ragingram2 <blazinram@gmail.com>
2020-06-08 21:56:19 +0200

    [Add] Timer

---
+++ Wasting Space/Assets/Indicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicator : MonoBehaviour
+{
+    private GameObject ship;
+    private GameObject station;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
