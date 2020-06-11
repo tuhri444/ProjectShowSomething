f77f37e8d6105f4308338340f41c8d44245173e9
Arjen de Aldrey <adealdrey3@gmail.com>
2020-06-11 16:40:14 +0200

    [ADD] Deadzone, item shrip when grabbing

---
+++ Wasting Space/Assets/Scripts/GameScripts/Deadzone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deadzone : MonoBehaviour
+{
+    private DeadzoneEffect effect;
+
+    private void Start()
+    {
+        effect = FindObjectOfType<DeadzoneEffect>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponentInParent<Ship>())
+        {
+            if (!effect.GetEffectOn())
+            {
+                effect.TurnEffectOn();
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponentInParent<Ship>())
+        {
+            if (effect.GetEffectOn())
+            {
+                effect.TurnEffectOff();
+            }
+        }
+    }
+
+}
