9757f6d342f5ea091e7ebd713c579282db34d427
SuperLeoni1 <leonilol11@gmail.com>
2020-06-12 10:37:06 +0200

    [ADD] Sound and implemented

---
+++ Wasting Space/Assets/Scripts/AnimationSound/PlaySoundOnEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOnEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource audioSource;
+
+    public AudioClip gloveOn;
+    public AudioClip helmetOn;
+
+    void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void PlayGloveOn()
+    {
+        audioSource.clip = gloveOn;
+        audioSource.Play();
+    }
+
+    void PlayHelmetOn ()
+    {
+        audioSource.clip = helmetOn;
+        audioSource.Play();
+    }
+}
