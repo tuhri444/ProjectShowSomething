36f573223aa306735b3a01a3211b86994ea34ee1
Rowan Ramsey <rowanramsey18@gmail.com>
2020-06-15 15:40:08 +0200

    [Change] Series of changes
    All Ship stats now move to the game scene, the leaderboard name input checks for bad words, fixed hte end menu kind of, stats change depending on the par tin customization

---
+++ Wasting Space/Assets/Scripts/NoNoWarning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoNoWarning : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject nonoWarning;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nonoWarning.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate(bool trualse)
+    {
+        nonoWarning.SetActive(trualse);
+    }
+}
