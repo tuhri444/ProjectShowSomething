14e3134c75f166815c07d753a2145f7563b6a983
Rowan Ramsey <rowanramsey18@gmail.com>
2020-06-17 19:45:18 +0200

    Auto stash before merge of "Development" and "origin/Development"

--- Wasting Space/Assets/LeaderboardPlace.cs
+++ Wasting Space/Assets/LeaderboardPlace.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class PlayerName : MonoBehaviour
+public class LeaderboardPlace : MonoBehaviour
 {
-    TMP_Text playerNameText;
     // Start is called before the first frame update
     void Start()
     {
-        playerNameText = GetComponent<TMP_Text>();
-        playerNameText.text = PlayerPrefs.GetString("CurrentPlayer");
+        
     }
 
     // Update is called once per frame
