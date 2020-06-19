b866a746bd00bbad1f84bfd52284bb2f9602d31b
Ingiomar <ingiomar@outlook.com>
2020-06-19 15:03:22 +0200

    Revert "Merge branch 'Development' of https://github.com/tuhri444/ProjectShowSomething into Development"
    This reverts commit af55d9f35fd9d4da1d50c2ec7be88617671aabe6, reversing
    changes made to 76fd83a1f91f36f614ec9773b03bff09766fb274.

--- Wasting Space/Assets/Scripts/UtilityScripts/ConnectToGoogle.cs
+++ Wasting Space/Assets/Scripts/UtilityScripts/ConnectToGoogle.cs
@@ -1,17 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class ConnectToGoogle : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        if (SceneManager.GetActiveScene().name.Equals("Start"))
-        {
-            Sheets.ConnectToGoogle();
-        }
+        Sheets.ConnectToGoogle();
     }
 
     // Update is called once per frame
