7b865c4e0192b3a3f4e2c4635d30a18bb7190ad0
Rowan Ramsey <rowanramsey18@gmail.com>
2020-06-23 19:28:51 +0200

    [Fixed] Leaderboard layout

--- Wasting Space/Assets/Scripts/UtilityScripts/Rating.cs
+++ Wasting Space/Assets/Scripts/UtilityScripts/Rating.cs
@@ -1,23 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rating : MonoBehaviour
 {
+    [SerializeField]
+    List<Sprite> ogImg = new List<Sprite>();
+
+    [SerializeField]
+    List<Sprite> clickedIcons = new List<Sprite>(); 
     int rating = -1;
-    public void OnClick()
+
+    public void OnClick(GameObject button)
     {
-        if (gameObject.name == "Good")
+        Debug.Log(button.name);
+        transform.GetChild(0).GetComponent<Image>().sprite = ogImg[0];
+        transform.GetChild(1).GetComponent<Image>().sprite = ogImg[1];
+        transform.GetChild(2).GetComponent<Image>().sprite = ogImg[2];
+        if (button.name == "Good")
         {
             rating = 2;
+            button.GetComponent<Image>().sprite = clickedIcons[2];
         }
-        else if(gameObject.name == "NotBad")
+        else if(button.name == "NotBad")
         {
             rating = 1;
+            button.GetComponent<Image>().sprite = clickedIcons[1];
         }
         else
         {
             rating = 0;
+            button.GetComponent<Image>().sprite = clickedIcons[0];
         }
     }
 
